1e4eca0ed5764101afa7219e130a6404bfe10498
Aryan Dhanuka <AryanDhanuka10>
2025-04-18 17:11:41 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 import time
 
 # Load the trained model
-model_path = "artifacts/model/sign_language_model.h5"
+model_path = "sign_language_model.h5"
 model = load_model(model_path)
 
 # Label mapping
